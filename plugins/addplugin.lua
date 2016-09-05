95b54209104d09d6d9177db5dfc67feef57653b1
mamad051 <mamad6032@gmail.com>
2016-09-05 21:30:55 +0430

    Update addplugin.lua

--- plugins/addplugin.lua
+++ plugins/addplugin.lua
@@ -6,14 +6,14 @@ local function run(msg, matches)
     text,b = text:gsub('^!+','')
   end
     if not is_sudo(msg) then
-    return 'ADD PLUGINS ONLY BY SUDO✘'
+    return 'فقط برای سودو ها✘'
   end
   local name = matches[2]
   local file = io.open("./"..name, "w")
   file:write(text)
   file:flush()
   file:close()
-  return "done✔"
+  return "انجام شد✔"
  end
  return {
   description = "a Usefull plugin for sudo !",
