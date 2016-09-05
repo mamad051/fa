9e10e5c02ad2303481f910025d53f12f08f43dc1
mamad051 <mamad6032@gmail.com>
2016-09-05 21:44:59 +0430

    Update aparat.lua

--- plugins/aparat.lua
+++ plugins/aparat.lua
@@ -1,5 +1,5 @@
 local function run(msg, matches)
- if matches[1]:lower() == 'aparat' then
+ if matches[1]:lower() == 'آپارات' then
   local url = http.request('http://www.aparat.com/etc/api/videoBySearch/text/'..URL.escape(matches[2]))
   local jdat = json:decode(url)
 
@@ -8,14 +8,14 @@ local function run(msg, matches)
   for i = 1, #items do
   text = text..'\n'..i..'- '..items[i].title..'  -  تعداد بازدید: '..items[i].visit_cnt..'\n    لینک: aparat.com/v/'..items[i].uid
   end
-  text = text..'\n\n@EN_Security'
+  text = text..'\n\nS $ S'
   return text
  end
 end
 
 return {
    patterns = {
-"^[#/!](aparat) (.*)$",
+"^(آپارات) (.*)$",
    },
    run = run
 }
