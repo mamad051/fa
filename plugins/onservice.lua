7d993c73861ce3f78a79fb732b6ed8b41ff036c2
mamad051 <mamad6032@gmail.com>
2016-09-06 13:40:16 +0430

    Update onservice.lua

--- plugins/onservice.lua
+++ plugins/onservice.lua
@@ -3,11 +3,11 @@ do
 local function run(msg, matches)
 local bot_id = our_id 
 local receiver = get_receiver(msg)
-    if matches[1] == 'leave' and is_admin1(msg) then
+    if matches[1] == 'خروج' and is_admin1(msg) then
        chat_del_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false)
 	   leave_channel(receiver, ok_cb, false)
     elseif msg.service and msg.action.type == "chat_add_user" and msg.action.user.id == tonumber(bot_id) and not is_admin1(msg) then
-       send_large_msg(receiver, 'ENS can be only added by @Eectrovirus', ok_cb, false)
+       send_large_msg(receiver, 'S&S can be only added by @agent_silver', ok_cb, false)
        chat_del_user(receiver, 'user#id'..bot_id, ok_cb, false)
 	   leave_channel(receiver, ok_cb, false)
     end
@@ -15,7 +15,7 @@ end
  
 return {
   patterns = {
-    "^[!/](leave)$",
+    "^خروج$",
     "^!!tgservice (.+)$",
   },
   run = run
